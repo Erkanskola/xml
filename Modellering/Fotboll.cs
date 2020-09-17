9fc9514a6c54763be62f05b1ea64034d2c7a179f
Erik Calissendorff <erik.calissendorff@learnet.se>
2020-09-17 13:50:30 +0200

    jag gjorde xml :)

---
+++ Modellering/Fotboll.cs
@@ -0,0 +1,8 @@
+public class Fotboll
+{
+    public int x;
+    public int y;
+    public float weight;
+    public int size;
+}
+
--- Modellering/Program.cs
+++ Modellering/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Modellering
 {
@@ -6,7 +9,62 @@ namespace Modellering
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fotboll fotboll = new Fotboll();
+
+            GetInput(fotboll);
+
+            System.Console.WriteLine($"Din fotboll ser ut så här:\nDen väger {fotboll.weight} Viktenheter.\nDen befinner sig i område {fotboll.x},{fotboll.y}.\nStorleken på fotbollen är {fotboll.size}.");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Fotboll));
+            using(FileStream file = File.Open("fotboll.xml", FileMode.OpenOrCreate)){
+                serializer.Serialize(file, fotboll);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void GetInput(Fotboll fotboll)
+        {
+            bool success = false;
+
+
+
+            while (!success)
+            {
+                System.Console.WriteLine("Skriv in fotbollens vikt");
+                success = float.TryParse(Console.ReadLine(), out float weight);
+                fotboll.weight = weight;
+            }
+
+            success = false;
+
+            while (!success)
+            {
+                System.Console.WriteLine("Skriv in fotbollens x-värde");
+                success = int.TryParse(Console.ReadLine(), out int x);
+                fotboll.x = x;
+            }
+
+            success = false;
+
+            while (!success)
+            {
+                System.Console.WriteLine("Skriv in fotbollens y-värde");
+                success = int.TryParse(Console.ReadLine(), out int y);
+                fotboll.y = y;
+            }
+
+            success = false;
+
+            while (!success)
+            {
+                System.Console.WriteLine("Skriv in fotbollens storlek");
+                success = int.TryParse(Console.ReadLine(), out int size);
+                fotboll.size = size;
+            }
+
         }
     }
 }
+
