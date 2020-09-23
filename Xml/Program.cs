b6889979424ef6eb9d20a514aa1d950d6dc06dda
Erik Calissendorff <erik.calissendorff@elev.ga.ntig.se>
2020-09-24 01:40:30 +0200

    Lite bättre kommentarer för lite tydligare extrapoäng :)

--- Xml/Program.cs
+++ Xml/Program.cs
@@ -11,10 +11,14 @@ namespace Modellering
         {
             Fotboll fotboll = new Fotboll();
 
+
+            //Här kallar jag en av mina metoder. Den består av flera loops som upprepas om spelaren inte lyckas göra success = true.
             GetInput(fotboll);
 
+            //Efter all input, skrivs värdena ut. Enklare på detta sätt än med en lista eller array, då det inte behöver vara dynamiskt.
             System.Console.WriteLine($"Din fotboll ser ut så här:\nDen väger {fotboll.weight} Viktenheter.\nDen befinner sig i område {fotboll.x},{fotboll.y}.\nStorleken på fotbollen är {fotboll.size}.");
 
+            //Xml-serializern som skapar eller uppdaterar en xml-fil med informationen som spelaren kommer skriva in i programmet.
             XmlSerializer serializer = new XmlSerializer(typeof(Fotboll));
             using(FileStream file = File.Open("fotboll.xml", FileMode.OpenOrCreate)){
                 serializer.Serialize(file, fotboll);
@@ -24,6 +28,7 @@ namespace Modellering
             Console.ReadLine();
         }
 
+        //Metoden för input med instruktioner för varje typ av input. Om success = true, får man gå vidare och skriva in resten av informationen. Om "!success", får man upp samma meddelande igen.
         static void GetInput(Fotboll fotboll)
         {
             bool success = false;
@@ -65,5 +70,18 @@ namespace Modellering
             }
 
         }
+
+        //Testar att göra en RNG för att man ska kunna "spela" spelet på något sätt. Tänker att man ska kalla metoden efter man skrivit in all information. Lite early WIP då jag fortfarande är ganska grön inom programmeringen och jag har inte lagt ned så mycket tid som jag velat.
+        static void kickBall(){
+
+
+            /*Random generator = new Random();
+
+            fotboll.x = generator.Next(0,11);
+            fotboll.y = generator.Next(0,11);
+
+            System.Console.WriteLine(fotboll.x, Fotboll.y);*/
+            
+         }
     }
 }
